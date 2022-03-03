aca7b96042c06799b472f0181f1345e541e186bd
ElizPN <pankevych.liza@gmail.com>
2022-03-03 19:06:00 +0100

    test component

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p className="ogogo">one</p>
+      <p className="tututut">two</p>
+      <p className="lolololo">three</p>
+      <input typeof="email"></input> <br />
+      <input typeof="radio"></input> <br />
+      <ul>
+        <li>kmm;mf</li>
+        <li>mkdm;dm;</li>
+        <li>nknn;s</li>
+        <li>kvenvfk</li>
+        <li>l;m;mdb</li>
+        <li>m;m;db</li>
+      </ul>
+      <table>
+        <tbody>
+          <tr>
+            <td>one</td>
+            <td>two</td>
+            <td>three</td>
+          </tr>
+
+          <tr>
+            <td>one</td>
+            <td>two</td>
+            <td>three</td>
+          </tr>
+        </tbody>
+        <tr>
+          <td>one</td>
+          <td>two</td>
+          <td>three</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
