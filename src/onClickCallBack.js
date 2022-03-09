ce2943b3293d256a33147901ae645a85c642b527
ElizPN <pankevych.liza@gmail.com>
2022-03-09 21:16:45 +0100

    callBack

--- src/onClickCallBack.js
+++ src/onClickCallBack.js
@@ -1,9 +1,30 @@
 export function OnClickCallBack() {
+  function showNumber(num) {
+    alert(num);
+  }
   return (
     <div>
-      <button>act1</button>
-      <button>act2</button>
-      <button>act3</button>
+      <button
+        onClick={() => {
+          showNumber(1);
+        }}
+      >
+        act1
+      </button>
+      <button
+        onClick={() => {
+          showNumber(2);
+        }}
+      >
+        act2
+      </button>
+      <button
+        onClick={() => {
+          showNumber(3);
+        }}
+      >
+        act3
+      </button>
     </div>
   );
 }
