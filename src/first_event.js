99dbba5c7dd6d3f502449c4943bedb91512ee6b1
ElizPN <pankevych.liza@gmail.com>
2022-03-09 21:17:36 +0100

    add event object

---
+++ src/first_event.js
@@ -0,0 +1,11 @@
+export function FirstEvent() {
+  function func(event) {
+    console.log(event.target); // where the click happened
+  }
+
+  return (
+    <div>
+      <button onClick={func}>act</button>
+    </div>
+  );
+}
