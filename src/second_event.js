f532f4d83a0a6a2e3a43760ec5ab2c407281f82f
ElizPN <pankevych.liza@gmail.com>
2022-03-10 21:46:22 +0100

    event

---
+++ src/second_event.js
@@ -0,0 +1,11 @@
+export function SecondEvent() {
+  function func(arg, event, arg2) {
+    console.log(arg, event, arg2);
+  }
+
+  return (
+    <div>
+      <button onClick={(event) => func("eee", event, "niiiii")}>act</button>
+    </div>
+  );
+}
