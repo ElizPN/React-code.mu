5bcd30759ed59de8f22ac2fee873af3f149dd7fa
ElizPN <pankevych.liza@gmail.com>
2022-03-10 21:45:55 +0100

    set state

---
+++ src/use_state.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react"; // useState is Hook
+
+export function State() {
+  const state = useState("prod"); // function call result - array with two elments (name of state and function for shanging name)
+  // as we have two elements of array  - we can use destructuring:
+
+  const [name, setName] = useState("prod");
+
+  function clickHendler() {
+    setName("New name");
+  }
+
+  return (
+    <div>
+      <span>{name}</span>
+      <button onClick={clickHendler}></button>BTN
+    </div>
+  ); // let's render our state with name to some page
+}
