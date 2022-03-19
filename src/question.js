59e586c26908d1df6e9f7e3b02b3365a1cabf568
ElizPN <pankevych.liza@gmail.com>
2022-03-19 20:08:04 +0100

    using if else with  check box

---
+++ src/question.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+export function Question() {
+  let [checked, setChecked] = useState(true);
+
+  let message;
+  if (checked) {
+    message = (
+      <div>
+        <h2>Ура, вам уже есть 18</h2>
+        <p>здесь расположен контент только для взрослых</p>
+      </div>
+    );
+  } else {
+    message = <p>увы, вам еще нет 18 лет:(</p>;
+  }
+
+  return (
+    <div>
+      <input
+        id="question"
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+      <label htmlFor="question">Вам уже есть 18?</label>
+      <div>{message} </div>
+    </div>
+  );
+}
