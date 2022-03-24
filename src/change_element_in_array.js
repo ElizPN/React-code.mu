06f31c9ce7cf528ebf9305941af26d938ac51282
ElizPN <pankevych.liza@gmail.com>
2022-03-24 16:55:13 +0100

    copy of array is created and first element is changed

--- src/change_element_in_array.js
+++ src/change_element_in_array.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
 export function ChangeElement() {
-  return <div>{"default"}</div>;
+  let [array, setArray] = useState([0, 2, 4, 6, 8]);
+
+  const newArr = Object.assign([], array);
+  let index = 0;
+  newArr[index] = "Zero";
+
+  return <div>{newArr}</div>;
 }
