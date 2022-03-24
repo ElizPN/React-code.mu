8efb34451a1bc8be251297df9690204d349b5a9a
ElizPN <pankevych.liza@gmail.com>
2022-03-24 11:26:44 +0100

    remove output to every paragraph; add destructuring for add new element to array

--- src/increase_array.js
+++ src/increase_array.js
@@ -3,12 +3,23 @@ import React, { useState } from "react";
 export function IncreaseArray() {
   let [array, setArray] = useState([1, 2, 3, 4, 5]);
 
-  let result = array.map((elem, index) => {
-    return <p key={index}>{elem}</p>;
-  });
+  // let result = array.map((elem, index) => {
+  //   return <p key={index}>{elem}</p>;
+  // });
 
-  let newArr = Object.assign([], array);
-  newArr.push(6);
+  // let newArr = Object.assign([], array);
+  // newArr.push(6);
 
-  return <div>{newArr}</div>;
+  return (
+    <div>
+      <p>{array}</p>
+      <button
+        onClick={() => {
+          setArray([...array, 6]);
+        }}
+      >
+        BTN
+      </button>
+    </div>
+  );
 }
