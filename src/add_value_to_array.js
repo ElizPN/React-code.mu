b3b8ccc9097e2e46944387f1b0eb7eaa6e14100b
ElizPN <pankevych.liza@gmail.com>
2022-03-25 12:42:12 +0100

    Displaying elements of array in paragraphs

--- src/add_value_to_array.js
+++ src/add_value_to_array.js
@@ -3,5 +3,9 @@ import React, { useState } from "react";
 export function AddValueToArray() {
   let [notes, setNotes] = useState([4, 3, 2, 5, 6, 4, 3]);
 
-  return <div>{notes}</div>;
+  let copy = notes.map((elem, index) => {
+    return <p key={index}>{elem}</p>;
+  });
+
+  return <div>{copy}</div>;
 }
