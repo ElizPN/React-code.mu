29d78707aa1b21c4c528a05daf8356d08fa8ccaf
ElizPN <pankevych.liza@gmail.com>
2022-03-25 12:55:01 +0100

    creating input with onChange attribute

--- src/add_value_to_array.js
+++ src/add_value_to_array.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 
 export function AddValueToArray() {
   let [notes, setNotes] = useState([4, 3, 2, 5, 6, 4, 3]);
+  let [value, setValue] = useState("");
 
   let copy = notes.map((elem, index) => {
     return <p key={index}>{elem}</p>;
   });
 
-  return <div>{copy}</div>;
+  function changeInput(event) {
+    setValue(event.target.value);
+  }
+
+  return (
+    <div>
+      {copy}
+      <input value={value} onChange={changeInput} />
+    </div>
+  );
 }
