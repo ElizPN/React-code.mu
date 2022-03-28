e16358bb59b682c04580fe8f9d9cb7b23df4a56b
ElizPN <pankevych.liza@gmail.com>
2022-03-28 11:45:42 +0200

    join two arrays into new one; set new array to state; create event in paragraph to call a function (handler that delates element from array )

--- src/delete_element_by_index.js
+++ src/delete_element_by_index.js
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 export function DeleteElementByIndex() {
   let [array, setArray] = useState([1, 2, 3, 4, 5, 6]);
 
-  let newArray = array.map((elem, index) => <p key={index}>{elem}</p>);
+  let newArray = array.map((elem, index) => (
+    <p key={index} onClick={() => deleteElement(index)}>
+      {elem}
+    </p>
+  ));
 
   function deleteElement(index) {
     let araryBeforeIndex = array.slice(0, index);
     let arrayAfterIndex = array.slice(index + 1);
 
-    return arrayAfterIndex;
+    return setArray([...araryBeforeIndex, ...arrayAfterIndex]);
   }
-  console.log(deleteElement(3));
 
   return <div>{newArray}</div>;
 }
