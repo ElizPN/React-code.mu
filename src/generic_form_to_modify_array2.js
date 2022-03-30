22ecfdce44083e080f9399a9fb2648b45f2e4483
ElizPN <pankevych.liza@gmail.com>
2022-03-30 16:59:24 +0200

    generic form to modify array (approach 2)

--- src/generic_form_to_modify_array2.js
+++ src/generic_form_to_modify_array2.js
@@ -7,15 +7,38 @@ export function GenericFormToModifyArray2() {
 
   const result = array.map((elem, index) => {
     return (
-      <p key={index} onClick={startEdit(index)}>
+      <p key={index} onClick={() => startEdit(index)}>
         {elem}
       </p>
     );
   });
   function startEdit(index) {
     setChangeIndex(index);
-    setValue(array[changeIndex]);
+    setValue(changeIndex === 0 || changeIndex ? array[changeIndex] : "");
   }
 
-  return <div>{result}</div>;
+  function changeHandler(event) {
+    setValue(event.target.value);
+    if (changeIndex) {
+      let arrayBeforeIndex = array.slice(0, changeIndex);
+      let arrayAfterIndex = array.slice(changeIndex + 1);
+      setArray([...arrayBeforeIndex, event.target.value, ...arrayAfterIndex]);
+    }
+  }
+
+  function blurHendlel(event) {
+    if (!changeIndex) {
+      setArray([...array, value]);
+    } else {
+      setChangeIndex(null);
+    }
+    setValue("");
+  }
+
+  return (
+    <div>
+      {result}
+      <input value={value} onChange={changeHandler} onBlur={blurHendlel} />
+    </div>
+  );
 }
