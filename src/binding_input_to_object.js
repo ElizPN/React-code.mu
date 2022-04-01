2b14cbdb69889a39a5cfbb65b2abb4c292aa7791
ElizPN <pankevych.liza@gmail.com>
2022-04-01 11:20:51 +0200

    binding event onChange to every inputs; set one function as handler

--- src/binding_input_to_object.js
+++ src/binding_input_to_object.js
@@ -9,11 +9,31 @@ const initObj = {
 export function BindingInputToObject() {
   const [obj, setObj] = useState(initObj);
 
+  //   function handleChangeProperty(prop, event) {
+  //     const copy = Object.assign({}, obj);
+  //     copy[prop] = event.target.value;
+  //     return setObj(copy);
+  //   }
+
+  function handleChangeProperty(prop, event) {
+    setObj({ ...obj, ...{ [prop]: event.target.value } });
+  }
+
   return (
     <div>
-      <input value={obj.prop1} />
-      <input value={obj.prop2} />
-      <input value={obj.prop3} /> <br />
+      <input
+        value={obj.prop1}
+        onChange={(event) => handleChangeProperty("prop1", event)}
+      />
+      <input
+        value={obj.prop2}
+        onChange={(event) => handleChangeProperty("prop2", event)}
+      />
+      <input
+        value={obj.prop3}
+        onChange={(event) => handleChangeProperty("prop3", event)}
+      />{" "}
+      <br />
       {obj.prop1}-{obj.prop2}-{obj.prop3}
     </div>
   );
