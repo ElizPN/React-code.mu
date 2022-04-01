9046032c992def7d9a339888e3d84826b1acb061
ElizPN <pankevych.liza@gmail.com>
2022-04-01 15:09:25 +0200

    binding inputs to object

--- src/binding_inputs_to_object2.js
+++ src/binding_inputs_to_object2.js
@@ -9,11 +9,31 @@ const initDate = {
 export function BindingInputsToObject2() {
   const [obj, setObj] = useState(initDate);
 
+  function handleChangeProperty(prop, event) {
+    const copy = Object.assign({}, obj);
+    copy[prop] = event.target.value;
+    setObj(copy);
+  }
+
   return (
     <div>
       <p>{obj.year}</p>
       <p>{obj.month}</p>
       <p>{obj.day}</p>
+      <br />
+
+      <input
+        value={obj.year}
+        onChange={(event) => handleChangeProperty("year", event)}
+      />
+      <input
+        value={obj.month}
+        onChange={(event) => handleChangeProperty("month", event)}
+      />
+      <input
+        value={obj.day}
+        onChange={(event) => handleChangeProperty("day", event)}
+      />
     </div>
   );
 }
