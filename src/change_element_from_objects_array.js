26487c8c91e654532369f4aafe3a270f8dc24170
ElizPN <pankevych.liza@gmail.com>
2022-04-02 11:02:11 +0200

    make strcture with objects array

---
+++ src/change_element_from_objects_array.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const initNotes = [
+  {
+    id: "GYi9G_uC4gBF1e2SixDvu",
+    prop1: "value11",
+    prop2: "value12",
+    prop3: "value13",
+  },
+  {
+    id: "IWSpfBPSV3SXgRF87uO74",
+    prop1: "value21",
+    prop2: "value22",
+    prop3: "value23",
+  },
+  {
+    id: "JAmjRlfQT8rLTm5tG2m1L",
+    prop1: "value31",
+    prop2: "value32",
+    prop3: "value33",
+  },
+];
+
+export function ChangeElementFromObjectsArray() {}
