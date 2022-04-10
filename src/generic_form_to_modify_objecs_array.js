13fcf73a56a8b6c5a5d11d0af741969f3a2b38a5
ElizPN <pankevych.liza@gmail.com>
2022-04-10 11:41:18 +0200

    generic form to modify objects array

--- src/generic_form_to_modify_objecs_array.js
+++ src/generic_form_to_modify_objecs_array.js
@@ -36,20 +36,68 @@ function getInitObj() {
 }
 
 export function GenericFormToModifyObjectsArray() {
-  const [notArray, setNoteArray] = useState(initNotes);
+  const [noteArray, setNoteArray] = useState(initNotes);
   const [editId, setEditId] = useState(null);
   const [obj, setObj] = useState(getInitObj());
 
-  const initNotesList = notArray.map((elem) => {
+  const initNotesList = noteArray.map((elem) => {
     return (
       <p key={elem.id}>
         <span>{elem.prop1}</span>
         <span>{elem.prop2}</span>
         <span>{elem.prop3}</span>
-        <button onClick={() => setEditId(elem.id)}></button>
+        <button onClick={() => setEditId(elem.id)}>Change item</button>
       </p>
     );
   });
 
-  return <div>{initNotesList}</div>;
+  function getValue(prop) {
+    if (editId) {
+      return noteArray.reduce(
+        (res, elem) => (elem.id === editId ? elem[prop] : res),
+        ""
+      );
+    } else {
+      return obj[prop];
+    }
+  }
+  function changeItem(prop, event) {
+    const newNoteArray = noteArray.map((elem) =>
+      elem.id === editId ? { ...elem, [prop]: event.target.value } : elem
+    );
+    if (editId) {
+      setNoteArray(newNoteArray);
+    } else {
+      setObj({ ...obj, [prop]: event.target.value });
+    }
+  }
+
+  function saveItem() {
+    if (editId) {
+      setEditId(null);
+    } else {
+      setNoteArray([...noteArray, obj]);
+      setObj(getInitObj());
+    }
+  }
+
+  return (
+    <div>
+      {initNotesList}
+      <input
+        value={getValue("prop1")}
+        onChange={(event) => changeItem("prop1", event)}
+      ></input>
+      <input
+        value={getValue("prop2")}
+        onChange={(event) => changeItem("prop2", event)}
+      ></input>
+      <input
+        value={getValue("prop3")}
+        onChange={(event) => changeItem("prop3", event)}
+      ></input>
+
+      <button onClick={saveItem}>Save item</button>
+    </div>
+  );
 }
