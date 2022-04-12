a566b1b51c8ca0eddba5c993a94cab522d57e4f4
ElizPN <pankevych.liza@gmail.com>
2022-04-12 16:54:01 +0200

    Update edit_list_elements_with_input.js

--- src/editing/edit_list_elements_with_input.js
+++ src/editing/edit_list_elements_with_input.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 
 const initNotes = [
-  { text: "note1", isEdit: true },
+  { text: "note1", isEdit: false },
   { text: "note2", isEdit: false },
   { text: "note3", isEdit: false },
 ];
 
 export function EditListElementsWithInput() {
   const [notesArray, setNotesArray] = useState(initNotes);
-  const [isEdit, setIsEdiit] = useState(false);
+
+  function startEdit(index) {
+    const copyNotesArray = Object.assign([], notesArray);
+    copyNotesArray[index].isEdit = true;
+    setNotesArray(copyNotesArray);
+  }
 
   const notesArrayLiist = notesArray.map((elem, index) => {
     let item;
     if (!elem.isEdit) {
-      item = <span>{elem.text}</span>;
+      item = <span onClick={() => startEdit(index)}>{elem.text}</span>;
     } else {
       item = <input value={elem.text} />;
     }
