508f84e2655df39f891d17475ba1179c6fa82e0e
ElizPN <pankevych.liza@gmail.com>
2022-04-18 10:29:12 +0200

    call editProd in onChange

--- src/components/product_child2.js
+++ src/components/product_child2.js
@@ -3,8 +3,18 @@ import React, { useState } from "react";
 export function ProductChild2({ id, name, cost, isEdit }) {
   return (
     <div>
-      name: {isEdit ? <input value={name} /> : <span>{name}</span>}
-      cost: {isEdit ? <input value={cost} /> : <span>{cost}</span>}
+      name:{" "}
+      {isEdit ? (
+        <input value={name} onChange={(event) => editProd(id, "name", event)} />
+      ) : (
+        <span>{name}</span>
+      )}
+      cost:{" "}
+      {isEdit ? (
+        <input value={cost} onChange={(event) => editProd(id, "cost", event)} />
+      ) : (
+        <span>{cost}</span>
+      )}
       {/* <button onClick={() => toggleMode(id)}>{isEdit ? "Save" : "Edit"}</button> */}
     </div>
   );
