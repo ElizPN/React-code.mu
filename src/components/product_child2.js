9610da083a9eb9e957d868b76054379ea127f365
ElizPN <pankevych.liza@gmail.com>
2022-04-18 09:48:13 +0200

    create button to toggle mode

--- src/components/product_child2.js
+++ src/components/product_child2.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-export function ProductChild2({ name, cost, isEdit }) {
+export function ProductChild2({ id, name, cost, isEdit }) {
   return (
     <div>
       <span> {name}</span>
       <span> {cost}</span>
       <span> {isEdit}</span>
+      <button onClick={() => toggleMode(id)}>Edit</button>
     </div>
   );
 }
